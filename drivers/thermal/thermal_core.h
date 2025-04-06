858788da8b7a9de70dcc947718fc1f455ca8db5e
duckyduckg <duckyduckg65@gmail.com>
2025-04-06 14:02:06 +0500

    drivers: thermal: Import Xiaomi Changes * From nitrogen-q-oss
    Signed-off-by: duckyduckg <duckyduckg65@gmail.com>

--- drivers/thermal/thermal_core.h
+++ drivers/thermal/thermal_core.h
@@ -3,6 +3,7 @@
  *  thermal_core.h
  *
  *  Copyright (C) 2012  Intel Corp
+ *  Copyright (C) 2020 XiaoMi, Inc.
  *  Author: Durgadoss R <durgadoss.r@intel.com>
  */
 
@@ -42,6 +43,15 @@ struct thermal_instance {
 	struct list_head cdev_node; /* node in cdev->thermal_instances */
 	unsigned int weight; /* The weight of the cooling device */
 };
+
+struct thermal_message {
+	bool message_ok;
+	const char *batt_array_size;
+	const char *batt_level_screen_on;
+	const char *batt_level_screen_off;
+};
+
+extern struct thermal_message *tm;
 
 #define to_thermal_zone(_dev) \
 	container_of(_dev, struct thermal_zone_device, device)
@@ -144,6 +154,8 @@ int of_thermal_get_ntrips(struct thermal_zone_device *);
 bool of_thermal_is_trip_valid(struct thermal_zone_device *, int);
 const struct thermal_trip *
 of_thermal_get_trip_points(struct thermal_zone_device *);
+int of_parse_thermal_message(void);
+void free_thermal_message(void);
 int of_thermal_aggregate_trip(struct thermal_zone_device *tz,
 			      enum thermal_trip_type type,
 			      int *low, int *high);
