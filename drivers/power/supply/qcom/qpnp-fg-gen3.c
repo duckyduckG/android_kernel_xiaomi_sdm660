Atom-X-Devs/scarlet_xiaomi_sdm660

qpnp-fg-gen3: Expose property online status
When device enters suspend mode in charging state, fg_psy_get_property tries to get the online status but fails to acquire it because of missing the required property. And dmesg notifies us with the following log:

[1.640123] FG: fg_psy_get_property: unsupported property 4

Add the missing property 4 with a case to define the online status of the driver.

Co-authored-by: Dede Dindin Qudsy <xtrymind@gmail.com>
Signed-off-by: Tashfin Shakeer Rhythm <tashfinshakeerrhythm@gmail.com>

drivers/power/supply/qcom/qpnp-fg-gen3.c
@@ -3841,6 +3841,11 @@ static int fg_psy_get_property(struct power_supply *psy,
 			return rc;
 		}
 		break;
+#ifdef CONFIG_MACH_LONGCHEER
+	case POWER_SUPPLY_PROP_ONLINE:
+		pval->intval = fg->online_status;
+		break;
+#endif
 	case POWER_SUPPLY_PROP_RESISTANCE:
 		rc = fg_get_battery_resistance(fg, &pval->intval);
 		break;

duckyduckG/android_kernel_xiaomi_sdm660

Signed-off-by: duckyduckg <duckyduckg65@gmail.com>

drivers/power/supply/qcom/qpnp-fg-gen3.c
@@ -3810,6 +3810,11 @@ static int fg_psy_get_property(struct power_supply *psy,
 			return rc;
 		}
 		break;
+#ifdef CONFIG_MACH_XIAOMI
+	case POWER_SUPPLY_PROP_ONLINE:
+		pval->intval = fg->online_status;
+		break;
+#endif
 	case POWER_SUPPLY_PROP_RESISTANCE:
 		rc = fg_get_battery_resistance(fg, &pval->intval);
 		break;