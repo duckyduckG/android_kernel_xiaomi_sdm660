19a203fb9bc2d57d1100cbc0eaefbba5ada8f354
duckyduckg <duckyduckg65@gmail.com>
2025-05-03 11:02:03 +0500

    [DNM][DIRTY]: kernel/power: disable suspend backoff

--- kernel/power/main.c
+++ kernel/power/main.c
@@ -24,7 +24,7 @@
 /*
  * Enable/Disable suspend back off logic attribute
  */
-static bool sbo_enabled = true;
+static bool sbo_enabled = false;
 
 /*
  * For how many percent an alive time is decayed
