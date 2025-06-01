620cdb4c1b730cf4c200decfecd62fae10336e12
duckyduckg <duckyduckg65@gmail.com>
2025-06-01 10:18:03 +0500

    kernel/sched/fair: bring back curr->policy for walt * use #ifndef to prevent bore scheduler breakage
    Signed-off-by: duckyduckg <duckyduckg65@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8541,6 +8541,9 @@ static void yield_task_fair(struct rq *rq)
 	clear_buddies(cfs_rq, se);
 #endif // CONFIG_SCHED_BORE
 
+#ifndef CONFIG_SCHED_BORE
+	if (curr->policy != SCHED_BATCH) {
+#endif
 	update_rq_clock(rq);
 	/*
 	 * Update run-time statistics of the 'current'.
@@ -8559,6 +8562,9 @@ static void yield_task_fair(struct rq *rq)
 	 * and double the fastpath cost.
 	 */
 	rq_clock_skip_update(rq);
+#ifndef CONFIG_SCHED_BORE
+	}
+#endif
 
 	set_skip_buddy(se);
 }
