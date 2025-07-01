639b211593a3db0193ae10ef37770398a9b8adcb
Rve27 <rve27github@gmail.com>
2025-07-01 22:26:27 +0500

    drivers: smb: bring in minimal support for bypass charging from Rve27
    this is from @Rve27
    
    it WORKS!
    
    from commit: https://github.com/duckyduckG/android_kernel_xiaomi_sdm845_419/commit/417c3d9f50925fc895afe6f57234f148780bcfe0#diff-cdde534fb90795ccf42e299ce2fb78f10701be7aab08dc1d50ca90a3b5cb0ebe
    
    Signed-off-by: duckyduckg <duckyduckg65@gmail.com>

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -48,6 +48,7 @@ struct g_nvt_data {
 extern struct g_nvt_data g_nvt;
 #endif
 
+static int bypass_charging = 0;
 static bool is_secure(struct smb_charger *chg, int addr)
 {
 	if (addr == SHIP_MODE_REG || addr == FREQ_CLK_DIV_REG)
@@ -1874,9 +1875,14 @@ int smblib_vbus_regulator_is_enabled(struct regulator_dev *rdev)
 int smblib_get_prop_input_suspend(struct smb_charger *chg,
 				  union power_supply_propval *val)
 {
-	val->intval
-		= (get_client_vote(chg->usb_icl_votable, USER_VOTER) == 0)
-		 && get_client_vote(chg->dc_suspend_votable, USER_VOTER);
+	if ((get_client_vote(chg->chg_disable_votable, BYPASS_VOTER) == 1)) {
+        	val->intval = 1;
+        } else if (bypass_charging) {
+        	val->intval = 2;
+        } else {
+        	val->intval = 0;
+        }
+
 	return 0;
 }
 
@@ -2228,7 +2234,16 @@ int smblib_set_prop_input_suspend(struct smb_charger *chg,
 	int rc;
 
 	/* vote 0mA when suspended */
-	rc = vote(chg->usb_icl_votable, USER_VOTER, (bool)val->intval, 0);
+	if (val->intval == 1) {
+        	rc = vote(chg->chg_disable_votable, BYPASS_VOTER, 1, 0);
+        	bypass_charging = 0;
+     	} else if (val->intval == 2) {
+        	rc = vote(chg->chg_disable_votable, BYPASS_VOTER, 0, 0);
+        	bypass_charging = 1;
+     	} else {
+        	rc = vote(chg->chg_disable_votable, BYPASS_VOTER, 0, 0);
+        	bypass_charging = 0;
+        }
 	if (rc < 0) {
 		smblib_err(chg, "Couldn't vote to %s USB rc=%d\n",
 			(bool)val->intval ? "suspend" : "resume", rc);
@@ -2311,6 +2326,8 @@ extern int hwc_check_india;
 int smblib_set_prop_system_temp_level(struct smb_charger *chg,
 				const union power_supply_propval *val)
 {
+	int system_temp_level;
+
 	if (val->intval < 0)
 		return -EINVAL;
 
@@ -2387,12 +2404,17 @@ int smblib_set_prop_system_temp_level(struct smb_charger *chg,
 		return vote(chg->chg_disable_votable,
 			THERMAL_DAEMON_VOTER, true, 0);
 
+	if (get_client_vote(chg->chg_disable_votable, BYPASS_VOTER) == 1) {
+		pr_info("%s bypass charging enabled",__FUNCTION__);
+        	return vote(chg->chg_disable_votable, THERMAL_DAEMON_VOTER, true, 0);
+        }
+
 	vote(chg->chg_disable_votable, THERMAL_DAEMON_VOTER, false, 0);
-	if (chg->system_temp_level == 0)
+	if (system_temp_level == 0)
 		return vote(chg->fcc_votable, THERMAL_DAEMON_VOTER, false, 0);
 
 	vote(chg->fcc_votable, THERMAL_DAEMON_VOTER, true,
-			chg->thermal_mitigation[chg->system_temp_level]);
+			chg->thermal_mitigation[system_temp_level]);
 	return 0;
 }
 
